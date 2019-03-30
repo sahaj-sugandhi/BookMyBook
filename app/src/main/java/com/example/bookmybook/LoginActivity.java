50f488255b98ee335dd8a2fa84ff89f6b8562a97
sahaj-sugandhi <49053600+sahaj-sugandhi@users.noreply.github.com>
2019-03-30 16:23:22 +0530

    "LogIn View"

---
+++ app/src/main/java/com/example/bookmybook/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.bookmybook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void clickedLogin(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickedSignup(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+}
